4bd28bd364442b8bd17fd60b21634927e5454288
IloveMonday <iloveyouchandlerbing@gmail.com>
2024-12-08 21:34:54 -0500

    html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -24,4 +25,13 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.ejs',
+        title: "The Weatherman",
+        filename: 'index.html',
+        inject: 'head',
+        scriptLoading: 'defer'
+    })
+  ]
 };
